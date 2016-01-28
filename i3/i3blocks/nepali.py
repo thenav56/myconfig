c15126b829bcfac541f9be13ac67ebd6e4d8de5b
Navin IR <navinayer56@gmail.com>
2016-01-28 09:35:42 +0545

    vim new plugins, i3 i3blocks added

--- i3/i3blocks/nepali.py
+++ i3/i3blocks/nepali.py
@@ -4,7 +4,7 @@ import sys,json, os
 import subprocess
 
 def get_cal():
-    return subprocess.check_output(["nepaliconv", "-bs", '-n', '-U', '-f', 'y.m.d']).decode('utf-8').strip()
+    return subprocess.check_output(["nepaliconv", "-bs", '-u', '-f', 'y.m.d']).decode('utf-8').strip()
 
 def main():
     print(get_cal())
