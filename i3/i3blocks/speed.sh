341c0df4827536cb83beacb57d1916bc7f468362
Navin IR <navinayer56@gmail.com>
2016-01-31 00:26:28 +0545

    i3 little clean, added speed

--- i3/i3blocks/speed.sh
+++ i3/i3blocks/speed.sh
@@ -2,8 +2,10 @@
 
 #rx1=$(cat /sys/class/net/wlan0/statistics/rx_bytes)
 rx1=$(cat ~/.config/i3blocks/rx)
-rx2=$(cat /sys/class/net/wlan0/statistics/rx_bytes)
-
+#rx2=$(cat /sys/class/net/enp4s0/statistics/rx_bytes)
+rx2=$(cat /sys/class/net/wlp2s0/statistics/rx_bytes)
+tx1=$(cat ~/.config/i3blocks/tx)
+tx2=$(cat /sys/class/net/wlp2s0/statistics/tx_bytes)
 echo "$rx2" > ~/.config/i3blocks/rx
-echo "↑" $( bc <<< "scale = 2; ($rx2-$rx1)/1024" ) "KB/s"
-
+echo "$tx2" > ~/.config/i3blocks/tx
+echo "" $( bc <<< "scale = 2; ($rx2-$rx1)/1024" ) "KB/s"  "" $( bc <<< "scale = 2; ($tx2-$tx1)/1024" ) "KB/s"
--- script/lock.sh
+++ script/lock.sh
@@ -2,5 +2,5 @@
 scrot /tmp/screen.png
 convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 [[ -f $1 ]] && convert /tmp/screen.png $1 -gravity center -composite -matte /tmp/screen.png
-i3lock  -i /tmp/screen.png
+i3lock  --image=/tmp/screen.png --pointer=default -f -e -d --inactivity-timeout=22
 rm /tmp/screen.png
