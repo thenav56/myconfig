e721ff76aa8749b056c4397a46e96285e2907ed7
Navin IR <navinayer56@gmail.com>
2016-02-01 21:21:07 +0545

    speed in kb/s and mb/s

--- i3/i3blocks/speed.sh
+++ i3/i3blocks/speed.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-#rx1=$(cat /sys/class/net/wlan0/statistics/rx_bytes)
 rx1=$(cat ~/.config/i3blocks/rx)
-#rx2=$(cat /sys/class/net/enp4s0/statistics/rx_bytes)
-rx2=$(cat /sys/class/net/wlp2s0/statistics/rx_bytes)
 tx1=$(cat ~/.config/i3blocks/tx)
+rx2=$(cat /sys/class/net/wlp2s0/statistics/rx_bytes)
 tx2=$(cat /sys/class/net/wlp2s0/statistics/tx_bytes)
 echo "$rx2" > ~/.config/i3blocks/rx
 echo "$tx2" > ~/.config/i3blocks/tx
-echo "" $( bc <<< "scale = 2; ($rx2-$rx1)/1024" ) "KB/s"  "" $( bc <<< "scale = 2; ($tx2-$tx1)/1024" ) "KB/s"
+upsd="($tx2-$tx1)/(5*1024)" #5 is for internal the script is runned
+dnsd="($rx2-$rx1)/(5*1024)"
+umag="KB/s"
+dmag="KB/s"
+if (( $upsd > 900 )) ; then
+tx1=$(cat ~/.config/i3blocks/tx)
+    upsd=$upsd/1024
+    umag="MB/s"
+fi
+if (( $dnsd > 900 )) ; then
+    dnsd=$dnsd/1024
+    dmag="MB/s"
+fi
+echo "" $( bc <<< "scale = 2; $dnsd" ) "$dmag"  "" $( bc <<< "scale = 2; $upsd" ) "$umag" 
