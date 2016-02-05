ac8ab456e27f9e8fcbdfbf043157e91d03cefb02
Navin IR <navinayer56@gmail.com>
2016-02-05 08:34:39 +0545

    future work

--- script/lock.sh
+++ script/lock.sh
@@ -2,5 +2,9 @@
 scrot /tmp/screen.png
 convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 [[ -f $1 ]] && convert /tmp/screen.png $1 -gravity center -composite -matte /tmp/screen.png
-i3lock  --image=/tmp/screen.png --pointer=default -f -e -d --inactivity-timeout=22
+#file="/sys/class/backlight/radeon_bl0/brightness"
+#old_brightness=$(cat "$file")
+#brightnessCon 10
+i3lock  --image=/tmp/screen.png --pointer=default -f -e --inactivity-timeout=22
 rm /tmp/screen.png
+#brightnessCon $old_brightness
